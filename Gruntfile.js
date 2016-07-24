259f24a6f941c9350a1aef711f2ac9420181ef79
marymhayes <marymhayes@hotmail.com>
2016-07-24 15:59:55 -0700

    completed assignment

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,7 +41,7 @@ module.exports = function (grunt) {
      },
      pages: {
        options: {
-         remote: 'git@github.com:your_github_user/your_webapp.git',
+         remote: 'git@github.com:marymhayes/wats4000-final-project.git',
          branch: 'gh-pages'
        }
      }
